bad2f726d631144c6c68ea4cbc9351469b3f10c6
AbhishekRanjan30 <abhishekranjan.a30@gmail.com>
2025-12-23 09:50:42 +0530

    updated code for JPA query

--- src/main/java/com/project/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/project/hospitalManagement/repository/PatientRepository.java
@@ -1,9 +1,37 @@
 package com.project.hospitalManagement.repository;
 
 import com.project.hospitalManagement.entity.Patient;
+import com.project.hospitalManagement.entity.type.BloodGroupType;
+import jakarta.transaction.Transactional;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Patient findByName(String name);
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+
+    @Query("SELECT p FROM Patient p WHERE p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType boodGroup);
+
+    @Query("SELECT p FROM Patient p WHERE p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("select p.bloodGroup, Count(p) from Patient p group by p.bloodGroup")
+    List<Object[]> countEachBloodGroupType();
+
+    @Query(value = "select * from patient", nativeQuery = true)
+    List<Patient> findAllPatient();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name=:name WHERE p.id=:id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
 }
--- src/test/java/com/project/hospitalManagement/PatientTest.java
+++ src/test/java/com/project/hospitalManagement/PatientTest.java
@@ -1,12 +1,15 @@
 package com.project.hospitalManagement;
 
 import com.project.hospitalManagement.entity.Patient;
+import com.project.hospitalManagement.entity.type.BloodGroupType;
 import com.project.hospitalManagement.repository.PatientRepository;
 import com.project.hospitalManagement.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -26,9 +29,38 @@ public class PatientTest {
 
     @Test
     public void testTransactionalMethods(){
+//
+//        Patient patient = patientService.getPatientById(1L);
+//        System.out.println(patient);
 
-        Patient patient = patientService.getPatientById(1L);
-        System.out.println(patient);
+     //   Patient patient1 = patientRepository.findByName("Abhishek Ranjan");
 
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1998,04,9),"vishwas@gmail.com");
+//        for(Patient patient : patientList){
+////            System.out.println(patient);
+//        }
+
+//        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+//        for(Patient patient : patientList){
+//            System.out.println(patient);
+//        }
+//
+//        List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1996,04,9));
+//        for(Patient patient : patientList){
+//            System.out.println((patient));
+//        }
+//
+//        List<Object[]> bloodGroupList = patientRepository.countEachBloodGroupType();
+//        for(Object[] objects : bloodGroupList){
+//            System.out.println(objects[0] + "  " + objects[1]);
+//        }
+
+//        List<Patient> patientList = patientRepository.findAllPatient();
+//        for(Patient patient : patientList){
+//            System.out.println(patient);
+//        }
+
+        int rowsUpdated = patientRepository.updateNameWithId("Abhishek", 1L);
+        System.out.println(rowsUpdated);
     }
 }
